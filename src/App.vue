541bf08c2f0bf09a5fe6e49889f15a6d7fef421e
Luciano <lucianowayand@hotmail.com>
2021-12-15 17:44:07 -0300

    criação de componente

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-
+  <primeiroComponente/>
 </template>
 
+<script>
+import primeiroComponente from './components/primeiroComponente.vue'
+
+export default {
+  name: 'App',
+  components:{
+    primeiroComponente
+  }
+    
+}
+
+</script>
 
