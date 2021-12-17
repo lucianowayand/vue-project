f23b601a15820e8705ad4c9d99b0d701975e5e69
Luciano <lucianowayand@hotmail.com>
2021-12-17 11:57:41 -0300

    adding props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="container" style="text-align: -webkit-center;">
+  <div style="text-align: -webkit-center;">
+    <Prop imageLink="https://spemer.com/img/works/vue/thumb.png" imageAlt="Vue logo"/>
     <info/>  
   </div>
   
 </template>
 
 <script>
+import Prop from './components/Prop.vue'
 import Info from './components/Info.vue'
 import Form from './components/Form.vue'
 import LifeCycle from './components/LifeCycle.vue'
@@ -14,6 +16,7 @@ import primeiroComponente from './components/primeiroComponente.vue'
 export default {
   name: 'App',
   components:{
+    Prop,
     Info,
     Form,
     LifeCycle,
@@ -28,7 +31,6 @@ export default {
     background-color: aqua;
     color: blueviolet;
     text-align: center;
-    margin-top: 20rem;
   }
   
 
---
+++ src/components/Prop.vue
@@ -0,0 +1,13 @@
+<template>
+    <img v-bind:src="imageLink" v-bind:alt="imageAlt">
+</template>
+
+<script>
+export default {
+    name:'Prop',
+    props:{
+        imageLink : String,
+        imageAlt : String
+    }
+}
+</script>
